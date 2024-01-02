bef3eb4f6e7de14e98267fb5ea32ba5a202603e9
docker-mobile <143889618+docker-mobile@users.noreply.github.com>
2024-01-02 15:31:22 +0330

    Update and rename paste-9cbf499a6a4a89cd.txt to p.bash

---
+++ p.bash
@@ -0,0 +1,5 @@
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+
+sudo dpkg -i cloudflared.deb && 
+
+sudo cloudflared service install eyJhIjoiM2ZiOTQ1MzFmYTcxMDMxZTUzOTM5NDZmNjU3M2JkZmQiLCJ0IjoiOWFhOWVjMzctMjIzYi00ZmIwLWE5MzUtNGM1YjE3MTc2OGYwIiwicyI6IlptVXlNalU0WW1FdFlUUmxZUzAwTVRFd0xUZzROakV0TWpobU5UVmlPRGhqTlRZNCJ9
